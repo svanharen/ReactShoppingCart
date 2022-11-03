159ab33895760646032155112c82674bf448aa4f
Sam VH <svanharen@my.bcit.ca>
2022-11-03 15:23:52 -0700

    First working Shopping cart without input count tracker

--- src/App.js
+++ src/App.js
@@ -1,23 +1,69 @@
-import logo from './logo.svg';
+
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+  
+  const products = ["Lentil", "Grain of Rice", "Black Bean", "Chocolate Chip", "Peppercorn"];
+  const [cart, setCart] = useState([]);
+
+  function makeForm(arr){
+    const productItems = arr.map((item, i) => 
+    <option key={i}>{item}</option>);
+    return <>
+      {productItems}
+    </>
+  }
+  var productInputCounts = {
+  }
+  function changeToMulti(arr, product){
+
+   let idx = arr.indexOf(product);
+   arr[idx] = `${product} X2`
+  }
+
+ 
+
+  function addToCart(){
+    const pToAdd = document.getElementById("product-form").value;
+    console.log(pToAdd);
+    const updatedCart = [ ...cart];
+    updatedCart.includes(pToAdd) ? changeToMulti(updatedCart, pToAdd) : updatedCart.push(pToAdd);
+    setCart(updatedCart);
+  }
+
+  function showCart(arr){
+    if(!arr || arr.length == 0) {
+      return <p>Add some items</p>
+    }
+    else{
+      const listItems = arr.map((item, i) => 
+      <li key={i}>{item}</li>);
+      return <ul>{listItems}</ul>
+    }
+  };
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Shopping Cart</h1>
       </header>
+      <main>
+      <section className="shopping-cart-form">
+  
+        <label htmlFor="product-form">Products: </label>
+          <select name="Products" id="product-form">
+            {makeForm(products)}
+          </select>
+        <button onClick=
+        {addToCart}>Add to Cart</button>
+      
+     </section>
+     <div className="cart-items">
+      <h2>Cart</h2>
+      {showCart(cart)}
+     </div>
+      </main>
     </div>
   );
 }
